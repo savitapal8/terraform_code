8420ea75d3da60462cc79d785b3925552efba187
Savita Pal <savita.pal08@gmail.com>
2022-07-26 12:03:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -153,9 +153,9 @@ resource "google_vertex_ai_dataset" "dataset" {
   display_name          = "terraform"
   metadata_schema_uri   = "gs://google-cloud-aiplatform/schema/dataset/metadata/image_1.0.0.yaml"
   region                = "us-central1"
-  encryption_spec{
+  #encryption_spec{
       #kms_key_name = google_kms_crypto_key.example-key.id
-  }
+  #}
 }
 
 #Vertex AI Featurestore
